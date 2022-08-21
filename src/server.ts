abfa1e9ffea63c6bff82a2d6db9e236b9ab7a4ad
hito-kotaro <tohi.work@gmail.com>
2022-08-22 08:17:43 +0900

    add db connection

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOSTNAME,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((err: any) => {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    console.log('db connection false');
+    return;
+  }
+  console.log('db connection success');
+});
+
+// app.get('/', (req: any, res: any) => {
+//   res.send('hello');
+// });
+
+app.get('/', (req: any, res: any) => {
+  connection.query('SELECT * FROM users', (error: any, results: any) => {
+    console.log(results);
+    res.send(results);
+  });
+});
+
+console.log(PORT);
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
